3554f27a180a73d53119d1b271e93488215bca05
seohee0112 <ghdtj7@naver.com>
2023-02-24 14:43:07 +0900

    feat : 마지막 next까지 체크

--- DesignPattern/iterator/arrayIterator.js
+++ DesignPattern/iterator/arrayIterator.js
@@ -9,3 +9,4 @@ console.log(arrIterator.next()); // { value: '지수', done: false }
 console.log(arrIterator.next()); // { value: '서히', done: false }
 console.log(arrIterator.next()); // { value: 'CS뿌시기', done: false }
 console.log(arrIterator.next()); // { value: '디자인패턴', done: false }
+console.log("마지막", arrIterator.next()); // 마지막 { value: undefined, done: true }
